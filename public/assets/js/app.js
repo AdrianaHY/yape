2bc76978836d1a2cf60903b9d405e24269c9d0fb
AdrianaHY <adriana.herya@gmail.com>
2017-07-18 14:51:20 -0500

    Comienzo de gulp y maquetación

---
+++ public/assets/js/app.js
@@ -0,0 +1,5 @@
+// function ಠ_ಠ45(){console.log("ಠ_ಠ45 mundo")}ಠ_ಠ45();
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+$('.carousel.carousel-slider').carousel({fullWidth: true});
