e17611a23f2ab5f317155667ba9c2239a4b03ec8
AdrianaHY <adriana.herya@gmail.com>
2017-07-21 10:57:42 -0500

    Validación de formulario, contador, obtención de codigo

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var levelUp = require('levelup');
 var morgan = require('morgan');
 var morganJson = require('morgan-json');
 var randomNumber = require('random-number');
-// var browserSync = require('browser-sync').create();
 var rutasPublic = {
   public: 'public',
   css : 'public/assets/css',
@@ -42,34 +41,6 @@ gulp.task('prepararJS', function(){
   .pipe(gulp.dest(rutasPublic.jsPublic))
 });
 
-//terminadas las tareas, se hacen las tareas de watch
-//tienen tres parámetros
-//para ello hay que installar browserSync
-
-// gulp.task('html-watch', ['prepararHTML'], function(done){
-//   browserSync.reload();
-//   done();
-// });
-//
-// gulp.task('scss-watch', ['prepararCSS'], function(done){
-//   browserSync.reload();
-//   done();
-// });
-//
-// gulp.task('js-watch', ['prepararJS'], function(done){
-//   browserSync.reload();
-//   done();
-// });
-//
-// //para levantar el servidor o la que estará observando los cambios
-//
-// gulp.task('serve', function(){
-//   browserSync.init({
-//     server:{
-//       baseDir: './public/views'
-//     }
-//   });
   gulp.watch(rutas.html);
   gulp.watch(rutas.scss);
   gulp.watch(rutas.js);
-// })
